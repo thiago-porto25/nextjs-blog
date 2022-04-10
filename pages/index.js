7be90c533f6e79a26edf2db71eb86733e56ae553
Thiago Porto <thiago.porto@d3.do>
2022-04-10 11:55:51 -0300

    change h1 in home

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi! I'm Thiago Porto a frontend developer from Brazil.</p>
+        <p>Hi! I'm Preview mode a frontend developer from Brazil.</p>
         <p>
           (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
